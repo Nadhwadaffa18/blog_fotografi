b53f023448b77b2360f3bc4b6ef71f0031d94377
indraagstin025 <indraagustin012@gmail.com>
2025-10-29 14:23:28 +0700

    fix link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,10 @@
-// Menunggu dokumen (HTML) selesai dimuat
-document.addEventListener('DOMContentLoaded', function() {
-    
-    // Pilih tombol berdasarkan ID
-    const menuToggle = document.getElementById('menu-toggle');
-    // Pilih menu navigasi berdasarkan ID
-    const navMenu = document.getElementById('nav-menu');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
 
-    // Tambahkan event listener 'click' pada tombol
-    menuToggle.addEventListener('click', function() {
-        // Toggle (tambah/hapus) class 'hidden' pada menu navigasi
-        // Tailwind akan menangani sisanya (menampilkan/menyembunyikan)
-        navMenu.classList.toggle('hidden');
-    });
+  const navMenu = document.getElementById("nav-menu");
+  console.log(navMenu);
 
+  menuToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("hidden");
+  });
 });
